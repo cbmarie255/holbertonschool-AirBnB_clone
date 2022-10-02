70fe80d147bd8d6f52b4c9cbb7e51b7c3fa77693
GoldLion1011 <yogirose79@gmail.com>
2022-10-01 19:33:55 -0700

    console 7+8

--- console.py
+++ console.py
@@ -3,47 +3,118 @@
 
 import cmd
 import sys
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """ implements command line functionality """
-    prompt = "(hbnb) "
+    prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        """ quits current shell """
+        """ quits the program """
         return True
 
     def do_EOF(self, arg):
-        """ quits current shell """
+        """ exits command line """
         return True
 
     def emptyline(self):
-        """ handles an empty line """
+        """ handles an empty line by doing nothing """
         pass
 
     def do_create(self, arg):
         """ creates new instance of Base Model, saves to JSON,
             prints id """
-        pass
+        arg = args.split()
+        if len(arg) < 1:
+            print("** class name missing **")
+            return False
+        if len(arg) > 1:
+            print("** too many arguments **")
+            return False
+        if arg[0] != 'BaseModel':
+            print('** class name missing **')
+            return False
+        new_obj = eval(str(args) + '()')
+        new_obj.save()
+        print(new_obj.id)
 
     def do_show(self, arg):
         """ prints str representation of an instance based
             on class name and id """
-        pass
+        arg = args.split()
+        if len(args) < 1:
+            print('** class name missing **')
+            return False
+        if arg[0] != 'BaseModel':
+            print('** class name missing **')
+            return False
+        if len(arg) < 2:
+            print('** instance id missing **')
+            return False
+        if len(arg) > 2:
+            print('** too many arguments **')
+            return False
 
     def do_destroy(self, arg):
         """ deletes an instance based on class name and id """
-        pass
+        arg = args.split()
+        if len(args) < 1:
+            print('** class name missing **')
+            return False
+        if arg[0] != 'BaseModel':
+            print('** class name missing **')
+            return False
+        if len(arg) < 2:
+            print('** instance id missing **')
+            return False
+        if len(arg) > 2:
+            print('** too many arguments **')
+            return False
+            """    obj_search = list_args[0] + '.' + list_args[1]
+                obj_all = storage.all()
+                if obj_search in obj_all:
+                    del obj_all[obj_search]
+                    storage.save()    """
 
     def do_all(self, arg):
-        """ prints str representation of all instances on the class name """
-        pass
+        """ prints str representation of all instances of the object """
+        arg = args.split()
+        if len(args) < 1:
+            print('** class name missing **')
+            return False
+        if arg[0] != 'BaseModel':
+            print('** class name missing **')
+            return False
+        if len(arg) < 2:
+            print('** instance id missing **')
+            return False
+        if len(arg) > 2:
+            print('** too many arguments **')
+            return False
 
     def do_update(self, arg):
         """ updates an instance based on class name and id
             by adding or updating an attribute """
-        pass
-
+        arg = args.split()
+        if len(args) == 0:
+            print('** class name missing **')
+            return False
+        if arg[0] != 'BaseModel':
+            print('** class name missing **')
+            return False
+        if len(arg) < 2:
+            print('** instance id missing **')
+            return False
+        if len(arg) < 3:
+            print('** attribute name missing **')
+            return False
+        if len(arg) < 4:
+            print('** value missing **')
+            return False
+        if len(arg) > 4:
+            print('** too many arguments **')
+            return False
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
